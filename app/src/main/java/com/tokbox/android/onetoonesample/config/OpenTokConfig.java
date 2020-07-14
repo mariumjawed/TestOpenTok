94ecf614d95d7386c24ce2b4fe828b71ab584113
huzaifa5820 <62013664+huzaifa5820@users.noreply.github.com>
2020-07-14 01:23:37 -0700

    test call

---
+++ app/src/main/java/com/tokbox/android/onetoonesample/config/OpenTokConfig.java
@@ -0,0 +1,29 @@
+package com.tokbox.android.onetoonesample.config;
+
+public class OpenTokConfig {
+
+
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+    // Replace with a generated Session ID
+
+
+    //USER_1
+   /* public static final String SESSION_ID = "2_MX40NjYzNTQ0Mn5-MTU5NDcxMTQwMjcwNH5IRFVuK3JEbmUydUY2dy9tQ050MHNEUWV-UH4";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NjYzNTQ0MiZzaWc9YTQ0MDkwYWI0MTQ5MDU3OTgzNDJjOGY5NDZkMTAzZmNkY2ZiMWRlZjpzZXNzaW9uX2lkPTJfTVg0ME5qWXpOVFEwTW41LU1UVTVORGN4TVRRd01qY3dOSDVJUkZWdUszSkVibVV5ZFVZMmR5OXRRMDUwTUhORVVXVi1VSDQmY3JlYXRlX3RpbWU9MTU5NDcxMTc3MCZyb2xlPW1vZGVyYXRvciZub25jZT0xNTk0NzExNzcwLjQ5MTgyMTA2NjEzODkzJmV4cGlyZV90aW1lPTE1OTUzMTY1NzAmY29ubmVjdGlvbl9kYXRhPW1hcmklNDB5b3BtYWlsLmNvbSZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PWZvY3Vz";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "46635442";
+    public static final String SECRET_KEY = "9dbcd8f76a4bb6be74baa65af7efc5d8706c4331";*/
+
+
+    // USER_2
+
+    public static final String SESSION_ID = "2_MX40NjYzNTQ0Mn5-MTU5MzUwMDM5MDU3MH5hYkxpTHlsMzkybnQ1MjBoVk5MaVhrdWV-UH4";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NjYzNTQ0MiZzaWc9ZDQ4MmQzNzJkZjUwOTU3YmZmYmZmNmQ3ZTJiMmQ1NTQwY2M2MjlkMzpzZXNzaW9uX2lkPTJfTVg0ME5qWXpOVFEwTW41LU1UVTVORGN4TVRRd01qY3dOSDVJUkZWdUszSkVibVV5ZFVZMmR5OXRRMDUwTUhORVVXVi1VSDQmY3JlYXRlX3RpbWU9MTU5NDcxMTc1MyZyb2xlPW1vZGVyYXRvciZub25jZT0xNTk0NzExNzUzLjQ1NjM2MTAyNDE0NjcmZXhwaXJlX3RpbWU9MTU5NTMxNjU1MyZjb25uZWN0aW9uX2RhdGE9bGF0aW5hX3VzZXIxJTQweW9wbWFpbC5jb20maW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD1mb2N1cw==";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "46635442";
+    public static final String SECRET_KEY = "9dbcd8f76a4bb6be74baa65af7efc5d8706c4331";
+
+}
